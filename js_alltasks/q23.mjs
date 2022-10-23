fb2b457a69b67ecee9f807836e28f0d6d8503a8e
Hamza <humzajaved68@gmail.com>
2022-10-24 02:10:05 +0500

    Update q23.mjs

--- js_alltasks/q23.mjs
+++ js_alltasks/q23.mjs
@@ -5,3 +5,20 @@
 // console.log(car == 'subaru')
 // • Look closely at your results, and make sure you understand why each line evaluates to True or False.
 // • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+
+let car = 'civic'
+let bike = 'ybr'
+let ship = 'titanic'
+let dog = 'puppy'
+let cat = 'persian'
+console.log(car == 'civic' ? true : false);
+console.log(bike == 'ybr' ? true : false);
+console.log(ship == 'titanic' ? true : false);
+console.log(dog == 'puppy' ? true : false);
+console.log(cat == 'persian' ? true : false);
+console.log(car !== 'civic' ? true : false);
+console.log(bike !== 'ybr' ? true : false);
+console.log(ship !== 'titanic' ? true : false);
+console.log(dog !== 'puppy' ? true : false);
+console.log(cat !== 'persian' ? true : false);
