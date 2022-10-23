577dbbbfc64c920064da54e03d5019f66089b2a0
Hamza <humzajaved68@gmail.com>
2022-10-23 20:52:59 +0500

    done

--- js_alltasks/q7.mjs
+++ js_alltasks/q7.mjs
@@ -1,2 +1,22 @@
 // Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
 //Be sure to enclose your operations in print statements to see the results.
+
+const num1 = 4;
+const num2 = 4;
+const num3 = num1 + num2;
+console.log(num3);
+
+const num4 = 12;
+const num5 = 4;
+const num6 = num4 - num5;
+console.log(num6);
+
+const num7 = 4;
+const num8 = 2;
+const num9 = num7 * num8;
+console.log(num9);
+
+const num10 = 16;
+const num11 = 2;
+const num12 = num10 / num11;
+console.log(num12);
