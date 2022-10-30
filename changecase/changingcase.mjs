6496a55f895469b58c874eeb6eac15fc03591223
Hamza <humzajaved68@gmail.com>
2022-10-30 17:52:00 +0500

    done

---
+++ changecase/changingcase.mjs
@@ -0,0 +1,11 @@
+// Changing Case
+// The toLowerCase() method converts a string to lowercase letters.
+// The toLowerCase() method does not change the original string.
+// The toUpperCase() method converts a string to uppercase letters.
+// The toUpperCase() method does not change the original string.
+
+let name = 'hamza'
+console.log(name.toLowerCase())
+console.log(name.toUpperCase())
+
+console.log(name[0].toUpperCase() + name.slice(1))
---
+++ loops/nestedloop.mjs
@@ -0,0 +1,6 @@
+for(let a = 0; a<5; a++){
+    console.log(a)
+    for(let j = 0; j<1; j++){
+        console.log("Hamza")
+    }
+}
