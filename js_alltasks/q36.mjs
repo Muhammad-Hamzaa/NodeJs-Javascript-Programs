334f6dfc92bf74a88b98877119432d715868e6f4
Hamza <humzajaved68@gmail.com>
2022-10-30 16:15:17 +0500

    done

---
+++ js_alltasks/q36.mjs
@@ -0,0 +1,11 @@
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+// The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+const size = Number('')
+const text = ""
+function make_shirt(){
+    return size + text
+}
+
+
+console.log(make_shirt(12, "Hello World"))
